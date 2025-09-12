cd1646485a936c28573f9c7335011e95c32a0f1a
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-12 22:03:06 +0330

    🌐 i18n(enum): add localized descriptions and display names to SampleEnum
    - add Description attributes with Persian translations for First and Second enum values
    - add Display attribute with Persian name for Second enum value

---
+++ CsharpGalexy.Console/Models/SampleEnum.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CsharpGalexy.Console.Models;
+
+public enum SampleEnum
+{
+    None = 0,
+
+    [Description("این مقدار اول است")]
+    First = 1,
+
+    [Description("این مقدار دوم است")]
+    [Display( Name ="مقدار دوم")]
+    Second = 2,
+
+    Third = 3
+}
