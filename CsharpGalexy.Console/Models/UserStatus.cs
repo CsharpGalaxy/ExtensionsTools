efe0c1e8dc6cf99bf1d4e198746306a1399f07c8
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-12 22:01:58 +0330

    ✨ feat(enum): add new enum with description and display attributes
    - add SampleEnum with Description and Display attributes for localization

--- CsharpGalexy.Console/Models/UserStatus.cs
+++ CsharpGalexy.Console/Models/UserStatus.cs
@@ -15,3 +15,18 @@ public enum UserStatus
     [Description("کاربر مسدود شده")]
     Blocked = 3
 }
+[Flags]
+enum FlagEnum { A = 1, B = 2, C = 4 }
+public enum SampleEnum
+{
+    None = 0,
+
+    [Description("این مقدار اول است")]
+    First = 1,
+
+    [Description("این مقدار دوم است")]
+    [Display( Name ="مقدار دوم")]
+    Second = 2,
+
+    Third = 3
+}
