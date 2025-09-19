70b0394fd76dba77e5f8e234e0241bd5df55866e
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-19 19:55:07 +0330

    ✨ feat(province): add province helper
    - add ProvinceHelper class with methods to get all provinces
    - implement GetAllProvinces method to return a list of all provinces in Iran
    - implement GetAllProvincesSorted method to return a sorted list of provinces

---
+++ CsharpGalexy.LibraryExtention/Province/ProvinceHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ProvinceHelper
+{
+    private static readonly string[] _provinces = new[]
+    {
+        "تهران",
+        "اصفهان",
+        "خراسان رضوی",
+        "فارس",
+        "آذربایجان شرقی",
+        "خوزستان",
+        "البرز",
+        "قم",
+        "یزد",
+        "گیلان",
+        "آذربایجان غربی",
+        "سیستان و بلوچستان",
+        "کردستان",
+        "خراسان شمالی",
+        "مازندران",
+        "هرمزگان",
+        "لرستان",
+        "قزوین",
+        "همدان",
+        "کهگیلویه و بویراحمد",
+        "اردبیل",
+        "خراسان جنوبی",
+        "زنجان",
+        "سمنان",
+        "ایلام",
+        "گلستان",
+        "بوشهر",
+        "مرکزی",
+        "چهارمحال و بختیاری",
+        "کرمان"
+    };
+
+    /// <summary>
+    /// دریافت لیست تمام استان‌های ایران
+    /// </summary>
+    /// <returns>آرایه‌ای از نام استان‌ها</returns>
+    public static string[] GetAllProvinces()
+    {
+        return (string[])_provinces.Clone(); // برای جلوگیری از تغییر خارجی
+    }
+
+    /// <summary>
+    /// [اختیاری] دریافت لیست مرتب شده بر اساس حروف الفبا
+    /// </summary>
+    public static string[] GetAllProvincesSorted()
+    {
+        return _provinces.OrderBy(x => x).ToArray();
+    }
+}
