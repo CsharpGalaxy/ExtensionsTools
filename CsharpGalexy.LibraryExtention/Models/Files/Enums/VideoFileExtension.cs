c6ff807bb6c40f90976d0cba210e89edcd3236f9
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-20 18:25:49 +0330

    ✨ feat(files): add video file extension enum   - create VideoFileExtension enum to define supported video file types   - include common video extensions like wmv, mpg, mpeg, mp4, avi, and flv

---
+++ CsharpGalexy.LibraryExtention/Models/Files/Enums/VideoFileExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace CsharpGalexy.LibraryExtention.File;
+
+public static partial class ValidateFiles
+{
+    private enum VideoFileExtension
+    {
+        none = 0,
+        wmv = 1,
+        mpg = 2,
+        mpeg = 3,
+        mp4 = 4,
+        avi = 5,
+        flv = 6
+    }
+}
