e899d0db9b6a1e95b28921f28439e27baf4860e8
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-20 18:25:39 +0330

    ✨ feat(file): add ImageFileExtension enum
    - add enum to manage image file extensions

---
+++ CsharpGalexy.LibraryExtention/Models/Files/Enums/ImageFileExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace CsharpGalexy.LibraryExtention.File;
+
+public static partial class ValidateFiles
+{
+    private enum ImageFileExtension
+    {
+        none = 0,
+        jpg = 1,
+        jpeg = 2,
+        bmp = 3,
+        gif = 4,
+        png = 5
+    }
+}
