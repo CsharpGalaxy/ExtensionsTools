3bd635ca39500acacf1a87cf532b229c0220a3f0
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-20 18:25:10 +0330

    ✨ feat(files): add DocDocxFileExtention enum
    - add DocDocxFileExtention enum to define available types of file extentions.

---
+++ CsharpGalexy.LibraryExtention/Models/Files/Enums/DocDocxFileExtention.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpGalexy.LibraryExtention.File;
+
+public static partial class ValidateFiles
+{
+    private enum DocDocxFileExtention
+    {
+        none = 0,
+        DOC = 1,
+        DOCX = 2
+    }
+}
