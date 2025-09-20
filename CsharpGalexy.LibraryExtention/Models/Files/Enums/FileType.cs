79c4c1add96b8c6bb56d579cc38c0421f30433c9
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-20 18:25:18 +0330

    ✨ feat(files): add FileType enum
    - introduce FileType enum to categorize different file types
    - support Image, Video, PDF, Text, DOC, DOCX, PPT, ZIP, and RAR

---
+++ CsharpGalexy.LibraryExtention/Models/Files/Enums/FileType.cs
@@ -0,0 +1,17 @@
+﻿namespace CsharpGalexy.LibraryExtention.File;
+
+public static partial class ValidateFiles
+{
+    public enum FileType
+    {
+        Image = 1,
+        Video = 2,
+        PDF = 3,
+        Text = 4,
+        DOC = 5,
+        DOCX = 6,
+        PPT = 7,
+        ZIP = 8,
+        RAR = 9
+    }
+}
