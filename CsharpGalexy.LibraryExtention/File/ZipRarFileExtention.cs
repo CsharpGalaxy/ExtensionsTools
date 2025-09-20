60cc4fef238834dc4d750d9b4e0970218f410ab3
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-09-20 18:25:03 +0330

    ✨ feat(file): add ZipRarFileExtention enum
    - introduce ZipRarFileExtention enum for zip and rar file type validation

---
+++ CsharpGalexy.LibraryExtention/File/ZipRarFileExtention.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpGalexy.LibraryExtention.File;
+
+public static partial class ValidateFiles
+{
+    private enum ZipRarFileExtention
+    {
+        none = 0,
+        ZIP = 1,
+        RAR = 2
+    }
+}
