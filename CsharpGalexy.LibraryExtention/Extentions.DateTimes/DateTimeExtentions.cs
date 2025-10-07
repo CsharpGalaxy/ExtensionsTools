2cade5c480e6e4c058bb27f0287eab5a489fdb4e
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-10-07 20:06:48 +0330

    ✨ feat(datetime): add persian date extensions
    - add persian date conversion methods
    - add persian day list and weekday enum
    - add method for getting persian name of day

--- CsharpGalexy.LibraryExtention/Extentions.DateTimes/DateTimeExtentions.cs
+++ CsharpGalexy.LibraryExtention/Extentions.DateTimes/DateTimeExtentions.cs
@@ -2,18 +2,74 @@
 
 using System;
 using System.Globalization;
-
+public enum WeekDay
+{
+    Saturday = 0,
+    Sunday = 1,
+    Monday = 2,
+    Tuesday = 3,
+    Wednesday = 4,
+    Thursday = 5,
+    Friday = 6
+}
 
 /// <summary>
 /// Extension methods for DateTime and DateTimeOffset
 /// </summary>
 public static class DateTimeExtensions
 {
+   
     private static readonly string[] PersianMonths =
     {
         "فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور",
         "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"
     };
+
+    public record PersianDayModel(int Id, string Name);
+    public static WeekDay GetTodayWeekDay(DateTime? dt)
+    {
+        // گرفتن روز فعلی از DateTime
+        var systemDay = dt.HasValue ? dt.Value.DayOfWeek : DateTime.Now.DayOfWeek;
+
+        // تبدیل به عدد صحیح
+        int dayIndex = (int)systemDay;
+
+        // تطبیق با WeekDay شما
+        // چون WeekDay از Saturday=0 شروع می‌شه، باید جابجا کنیم
+        // یعنی Sunday=1 در System باید بشه Sunday=1 در WeekDay و Saturday=6 باید بشه Saturday=0
+
+        // تبدیل از System.DayOfWeek به WeekDay
+        WeekDay today = (WeekDay)(((dayIndex + 1) % 7));
+
+        return today;
+    }
+
+
+    public static IReadOnlyList<PersianDayModel> PersianDayList =>
+    [
+        new((int)DayOfWeek.Saturday, "شنبه"),
+        new((int)DayOfWeek.Sunday, "یک‌شنبه"),
+        new((int)DayOfWeek.Monday, "دوشنبه"),
+        new((int)DayOfWeek.Tuesday, "سه‌شنبه"),
+        new((int)DayOfWeek.Wednesday, "چهار‌شنبه"),
+        new((int)DayOfWeek.Thursday, "پنج‌شنبه"),
+        new((int)DayOfWeek.Friday, "جمعه")
+    ];
+
+    public static IReadOnlyList<(WeekDay Id, string Name)> PersianOfDayList =>
+ [
+     new(WeekDay.Saturday, "شنبه"),
+        new(WeekDay.Sunday, "یک‌شنبه"),
+        new(WeekDay.Monday, "دوشنبه"),
+        new(WeekDay.Tuesday, "سه‌شنبه"),
+        new(WeekDay.Wednesday, "چهار‌شنبه"),
+        new(WeekDay.Thursday, "پنج‌شنبه"),
+        new(WeekDay.Friday, "جمعه")
+ ];
+    public static string GetNameOfDay(WeekDay WeekDay)
+    {
+        return PersianOfDayList.FirstOrDefault(_ => _.Id == WeekDay)!.Name;
+    }
     #region Persian (Shamsi) Date Conversions
 
     /// <summary>
