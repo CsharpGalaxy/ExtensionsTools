a70e44eaec75677f316727c5091dbbe7a1a80b12
abolfazl Shabani <57566177+abolfazlshs80@users.noreply.github.com>
2025-10-29 20:03:54 +0330

    ✨ feat(export): add excel export functionality
    - introduce ExcelExportHelper for exporting data to excel format
    - implement ExportToExcel method to convert list of objects to excel byte array
    - use ClosedXML library for excel operations

---
+++ CsharpGalaxy.LibraryExtension.Export/Helper/ExcelExportHelper.cs
@@ -0,0 +1,40 @@
+﻿namespace CsharpGalaxy.LibraryExtension.Export.Helper;
+
+using ClosedXML.Excel;
+using System.Data;
+using System.Reflection;
+
+public static class ExcelExportHelper
+{
+    public static byte[] ExportToExcel<T>(List<T> data, string sheetName = "Sheet1")
+    {
+        using var workbook = new XLWorkbook();
+        var worksheet = workbook.Worksheets.Add(sheetName);
+
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        // Header
+        for (int i = 0; i < properties.Length; i++)
+        {
+            worksheet.Cell(1, i + 1).Value = properties[i].Name;
+            worksheet.Cell(1, i + 1).Style.Font.Bold = true;
+        }
+
+        // Rows
+        for (int row = 0; row < data.Count; row++)
+        {
+            for (int col = 0; col < properties.Length; col++)
+            {
+                var cellValue = properties[col].GetValue(data[row]);
+                worksheet.Cell(row + 2, col + 1).Value = cellValue?.ToString() ?? "";
+
+            }
+        }
+
+        worksheet.Columns().AdjustToContents();
+
+        using var stream = new MemoryStream();
+        workbook.SaveAs(stream);
+        return stream.ToArray();
+    }
+}
